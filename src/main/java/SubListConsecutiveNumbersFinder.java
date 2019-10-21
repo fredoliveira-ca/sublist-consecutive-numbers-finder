6c105789b6781eafc41cff84c0e51ed962cd204c
Fred <fredoliveira.ce@gmail.com>
2019-10-21 17:01:54 -0300

    Adding println.

--- src/main/java/SubListConsecutiveNumbersFinder.java
+++ src/main/java/SubListConsecutiveNumbersFinder.java
@@ -11,6 +11,7 @@ public class SubListConsecutiveNumbersFinder {
 
     public static void main(String[] args) {
 
+        System.out.println("Inform the array size: ");
         int arraySize = scanner.nextInt();
         int[] input = new int[arraySize];
 
